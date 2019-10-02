f46f3cdc6689efb4ff30055ac7d8de45f82f114e
marcisvil <44918188+marcisvil@users.noreply.github.com>
2019-10-02 19:33:34 +0300

    izmēģinājums

---
+++ src/lv/jg/lesson1/HelloWorld.java
@@ -0,0 +1,12 @@
+package lv.jg.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 67;
+        int vecums = 33;
+        int attieciba = age/vecums;
+        System.out.println(attieciba);
+
+    }
+
+}
