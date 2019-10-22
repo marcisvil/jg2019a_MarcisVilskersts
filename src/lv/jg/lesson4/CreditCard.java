898d96240cac6ce208ec9a8458e21e28aa0b4859
marcisvil <44918188+marcisvil@users.noreply.github.com>
2019-10-22 22:29:25 +0300

    Homework

---
+++ src/lv/jg/lesson4/CreditCard.java
@@ -0,0 +1,4 @@
+package lv.jg.lesson4;
+
+public class CreditCard {
+}
---
+++ src/lv/jg/lesson4/UserLoginServiceTest.java
@@ -0,0 +1,33 @@
+package lv.jg.lesson4;
+
+
+public class UserLoginServiceTest {
+
+    public void test() {
+        UserLoginService victim = new UserLoginService();
+        User loginVictim = new User("SweetLoginName", "hiddenWord");
+
+        String expectedResult = "Login successful";
+        String actualResult = victim.login("hiddenWord", loginVictim);
+
+
+        check(expectedResult, actualResult);
+    }
+
+    public void check(String expectedResult, String actualResult) {
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("Test successful");
+        } else {
+            System.out.println("Test NOT successful");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        UserLoginServiceTest userLoginServiceTest = new UserLoginServiceTest();
+        userLoginServiceTest.test();
+    }
+
+
+}
